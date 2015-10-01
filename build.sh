6c8c29cfe6b7a94d9ea06f44231f19e362f740dc
marcelocorreia <marcelo.correia@starvisitor.com>
2015-10-01 23:51:30 +1000

    updating..

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker build -t marcelocorreia/mule:3.7.0-java8 .
-sudo docker run --rm marcelocorreia/mule:3.7.0-java8 
+sudo docker build -t marcelocorreia/mule:3.6.1
+sudo docker run --rm marcelocorreia/mule:3.6.1
